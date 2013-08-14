cff27575e55b35fa071ccda50a949e9c518c7eab
Enrico <the.enrico.els@gmail.com>
2013-08-14 15:21:18 +0200

    view made

--- Ideas4AppsHub.MVC/Controllers/AdminController.cs
+++ Ideas4AppsHub.MVC/Controllers/AdminController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ideas4AppsHub.Repositories;
+using Ideas4AppsHub.Domain;
 
 namespace Ideas4AppsHub.MVC.Controllers
 {
@@ -10,6 +12,7 @@ namespace Ideas4AppsHub.MVC.Controllers
     {
         //
         // GET: /Admin/
+        private BusinessRepository _businessRepository = new BusinessRepository();
 
         public ActionResult Index()
         {
@@ -20,9 +23,21 @@ namespace Ideas4AppsHub.MVC.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult CreateBusiness(string name) {
+        public ActionResult UpdateBusiness() {
             return View();
+        }
+
+        [AllowAnonymous]
+        public JsonResult CreateBusiness(string name, string description, string telephone, string address1, string address2, string address3, string postalcode, string status, string businesshours, string tags, string weburl, string category, bool active) 
+        {
+            _businessRepository.CreateBusiness(name);
+
+            var data = new
+            {
+                isOk = true
+            };
+
+            return new JsonResult { Data = data };
         }
     }
 }
---
+++ Ideas4AppsHub.Repositories/BusinessRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ideas4AppsHub.Data;
+using Ideas4AppsHub.Domain;
+
+namespace Ideas4AppsHub.Repositories
+{
+    public class BusinessRepository
+    {
+        public void CreateBusiness(string name)
+        {
+            using (var model = new ideas4appsEntities())
+            {
+                var newBusiness = new Ideas4AppsHub.Data.business()
+                {
+                    name = name,
+                };
+                model.businesses.Add(newBusiness);
+                model.SaveChanges();
+            }
+        }
+
+    }
+}
