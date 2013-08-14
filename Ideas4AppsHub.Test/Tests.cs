aa5aefd2cfa5fe46695d6f25d5ca76ab09a64eba
PhillipKemp <phillipkemp007@gmail.com>
2013-08-14 11:09:18 +0200

    Commit

--- Ideas4AppsHub.Test/Tests.cs
+++ Ideas4AppsHub.Test/Tests.cs
@@ -1,11 +1,57 @@
-﻿using System;
+﻿using Ideas4AppsHub.Domain;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Ideas4AppsHub.Test
 {
-    class Tests
+    [TestFixture]
+
+    public class Tests
     {
+        [Test]
+        public void Can_create_appEntity()
+        {
+            var appEntity = new AppEntity()
+            {
+                Id = 1,
+                TelephoneNumber = "018 932783 89",
+                Name = "Apps4Everyone",
+                Description = "First App",
+                LastUpdate = DateTime.Now,
+                Status = new Status()
+            };
+
+            Assert.That(appEntity, Is.Not.Null);
+        }
+
+        [Test]
+        public void Can_create_businessAppEntity()
+        {
+            var business = new Business()
+            {
+                BusinessHours = "08:00 - 17:00",
+                Name = "KFC",
+                Tags = "Chicken|Fast Foods|Drive Thru",
+                WebUrl = "http://www.kfc.co.za",
+                Address = new Address(),
+                Photo = new Photo(),
+                GPS = new GPS(),
+                Category = new Category(),
+                Status = Status.New,
+                Active = true
+            };
+
+            Assert.That(business, Is.Not.Null);        
+            
+
+        }
+
+
+
+
+
     }
 }
